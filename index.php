b6e318c94be6c7a57568149d5aedc67bcacadde0
Pascal Chevrel <pascal.chevrel@free.fr>
2018-07-12 13:53:33 +0200

    commit the right file

--- index.php
+++ index.php
@@ -1,83 +1,78 @@
-<?php
-$cache_time   = 18000; // 5 hours
-$cache_source = 'https://product-details.mozilla.org/1.0/firefox_versions.json';
-$cache_file   = 'firefox_versions_local.json';
+<?php include 'data.php'?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Release Links and Queries</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <style>
 
-// Serve from the cache if it is younger than $cache_time
-$cache_ok = file_exists($cache_file) && time() - $cache_time < filemtime($cache_file);
+    </style>
+</head>
+<body>
+    <a href="https://bugzilla.mozilla.org/page.cgi?id=release_tracking_report.html">Release Tracking Report</a>
 
-if (! $cache_ok) {
-	file_put_contents($cache_file, file_get_contents($cache_source, true));
-}
+    <table id="version_numbers" class="table table-striped table-bordered table-sm w-auto" style="margin-left: 1em">
+        <tbody>
+            <tr>
+                <th>Nightly</th>
+                <td><?=NIGHTLY?></td>
+            </tr>
+            <tr>
+                <th>Dev Edition</th>
+                <td><?=DEV_EDITION?></td>
+            </tr>
+            <tr>
+                <th>Beta</th>
+                <td><?=BETA?></td>
+            </tr>
+            <tr>
+                <th>Release</th>
+                <td><?=RELEASE?></td>
+            </tr>
+            <tr>
+                <th>Old ESR</th>
+                <td><?=ESR?></td>
+            </tr>
+            <tr>
+                <th>ESR</th>
+                <td><?=ESR_NEXT?></td>
+            </tr>
+        </tbody>
+    </table>
 
-$firefox_versions = json_decode(file_get_contents($cache_file), true);
+    <div class="container">
+        <div class="row">
+        <div class="col">
+            <h3>NIGHTLY</h3>
+            <ul>
+                <li><a href="">Regressions</a></li>
+                <li><a href="<?=$relnotes_nightly?>">Release Note Requests</a></li>
+                <li>--</li>
+                <li><a href="<?=$malfunction_nightly?>">Software defect (crash, leak, assertion…)</a></li>
+            </ul>
+        </div>
 
-define('ESR', $firefox_versions["FIREFOX_ESR"]);
-define('ESR_NEXT', $firefox_versions["FIREFOX_ESR_NEXT"]);
-define('NIGHTLY', $firefox_versions["FIREFOX_NIGHTLY"]);
-define('DEV_EDITION', $firefox_versions["FIREFOX_DEVEDITION"]);
-define('BETA', $firefox_versions["LATEST_FIREFOX_RELEASED_DEVEL_VERSION"]);
-define('RELEASE', $firefox_versions["LATEST_FIREFOX_VERSION"]);
+        <div class="col">
+            <h3>BETA</h3>
+            <ul>
+                <li><a href="">Regressions</a></li>
+                <li><a href="<?=$relnotes_beta?>">Release Note Requests</a></li>
+                <li><a href="<?=$uplift_beta?>">Uplift requests</a></li>
+                <li><a href="<?=$malfunction_beta?>">Software defect (crash, leak, assertion…)</a></li>
+            </ul>
+        </div>
 
-$main_nightly = (int) NIGHTLY;
-$main_beta 	  = (int) BETA;
-$main_release = (int) RELEASE;
-
-$stub_search_bz = 'https://bugzilla.mozilla.org/buglist.cgi?query_format=advanced';
-
-// Release notes
-$relnotes_stub =
-	$stub_search_bz
-	. '&o1=equals'
-	. '&v1=%3F'
-	. '&o2=anywords'
-	. '&v2=affected%2Cfixed%2Cverified%2Cfix-optional'
-	. '&f1=cf_tracking_firefox_relnote'
-	. '&f2=cf_status_firefox';
-
-$relnotes_nightly = $relnotes_stub . $main_nightly;
-$relnotes_beta    = $relnotes_stub . $main_beta;
-$relnotes_release = $relnotes_stub . $main_release;
-
-// Uplifts requests
-$uplift_stub 	= $stub_search_bz . '&o1=substring&f1=flagtypes.name';
-$uplift_beta 	= $uplift_stub . '&v1=approval-mozilla-beta%3F';
-$uplift_release = $uplift_stub . '&v1=approval-mozilla-release%3F';;
-
-
-// Ryan query: crash, leak, security, dataloss, assertion
-$malfunction_stub =
-	$stub_search_bz
-	. '&o5=anywordssubstr'
-	. '&j2=OR'
-	. '&o1=anywords'
-	. '&v5=%2B%20%3F'
-	. '&o4=substring'
-	. '&v1=affected%20optional'
-	. '&v4=sec'
-	. '&v6=%2B%20%3F%20blocking'
-	. '&o3=anywords'
-	. '&v3=crash%20regression%20leak%20topcrash%20assertion%20dataloss'
-	. '&resolution=FIXED'
-	. '&o6=anywordssubstr'
-	. '&f4=bug_group'
-	. '&f3=keywords'
-	. '&f2=OP'
-	. '&f5=cf_blocking_fennec'
-	. '&f7=CP';
-
-$malfunction_nightly =
-	$malfunction_stub
-	. '&f6=cf_tracking_firefox' . $main_nightly
-	. '&f1=cf_status_firefox' . $main_nightly;
-
-$malfunction_beta =
-	$malfunction_stub
-	. '&f6=cf_tracking_firefox' . $main_beta
-	. '&f1=cf_status_firefox' . $main_beta;
-
-$malfunction_release =
-	$malfunction_stub
-	. '&f6=cf_tracking_firefox' . $main_release
-	. '&f1=cf_status_firefox' . $main_release;
-
+        <div class="col">
+            <h3>RELEASE</h3>
+            <ul>
+                <li><a href="">Regressions</a></li>
+                <li><a href="<?=$relnotes_release?>">Release Note Requests</a></li>
+                <li><a href="<?=$uplift_release?>">Uplift requests</a></li>
+                <li><a href="<?=$malfunction_release?>">Software defect (crash, leak, assertion…)</a></li>
+            </ul>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
