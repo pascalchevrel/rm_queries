48548942adc41af8cd0285219503ba3efcd787e0
Pascal Chevrel <pascal.chevrel@free.fr>
2018-10-03 12:09:13 +0200

    first betas are dev edition only, add link logic to manage devedition hg tags

--- index.php
+++ index.php
@@ -61,12 +61,23 @@
         <ul>
 <?php
 for ($i = 2; $i <= $last_beta + 1; $i++) {
-    $hg_link =
-        'https://hg.mozilla.org/releases/mozilla-beta/pushloghtml?fromchange=FIREFOX_63_0b'
-        . ($i-1)
-        . '_RELEASE&amp;tochange='
-        . ($i-1 == $last_beta ? 'tip': 'FIREFOX_63_0b'. $i . '_RELEASE');
+    $beta_previous = ($i - 1) < 3 ? 'DEVEDITION_' : 'FIREFOX_';
+    $beta_current_type = $i < 3 ? 'DEVEDITION_' : 'FIREFOX_';
 
+    $beta_previous .= $main_beta . '_0b' . ($i-1) . '_RELEASE';
+    $beta_current = ($i-1 == $last_beta)
+        ? 'tip'
+        : $main_beta . '_0b' . $i . '_RELEASE';
+
+    if ($beta_current != 'tip') {
+        $beta_current = $beta_current_type . $beta_current;
+    }
+
+    $hg_link =
+        'https://hg.mozilla.org/releases/mozilla-beta/pushloghtml?fromchange='
+        . $beta_previous
+        . '&amp;tochange='
+        . $beta_current;
     print '            <li>' . $link($hg_link,'Beta' . $i, $title = false ) . "</li>\n";
 }
 ?>
